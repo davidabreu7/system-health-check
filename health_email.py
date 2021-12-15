6436db8bea3f563fb896686f355497c9cdd116f7
David Abreu <62473543+davidabreu7@users.noreply.github.com>
2021-12-14 22:16:41 -0300

    Update health_email.py

--- health_email.py
+++ health_email.py
@@ -5,6 +5,6 @@ def send_email(body=""):
     smtp_server = smtplib.SMTP("smtp.office365.com", 587)
     smtp_server.ehlo()
     smtp_server.starttls()
-    smtp_server.login("david.abreu@outlook.com", "dv84em19jl87")
+    smtp_server.login("david.abreu@outlook.com", "password")
     smtp_server.sendmail(from_add, to_add, body)
     smtp_server.quit()
